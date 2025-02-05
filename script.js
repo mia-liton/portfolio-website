b184e90d537e5a6491782790003a86a5dbcda97e
mia-liton <litonmioa@gmail.com>
2025-02-05 17:56:49 +0600

    message

--- script.js
+++ script.js
@@ -14,3 +14,40 @@ resumeBtns.forEach((btn, idx) => {
         resumeDetails[idx].classList.add('active');
     });
 });
+
+const arrowRight = document.querySelector('.portfolio-box .navigation button .arrow-right');
+const arrowLeft = document.querySelector('.portfolio-box .navigation button .arrow-left');
+
+let index = 0;
+
+const activePortfolio = () => {
+    const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
+
+    imgSlide.computedStyleMap.transform = 'translateX(clac(${index * -100}%) - ${index * 2}rem));'
+}
+
+arrowRight.addEventListener('click', () => {
+    if (index < 4 ){
+        index++;
+        arrowLeft.classList.remove('disabled');
+    }
+    else {
+        index = 5;
+        arrowRight.classList.add('disabled');
+    }
+
+    activePortfolio();
+});
+
+arrowLeft.addEventListener('click', () => {
+    if (index > 1 ){
+        index--;
+        arrowRight.classList.remove('disabled');
+    }
+    else {
+        index = 0;
+        arrowLeft.classList.add('disabled');
+    }
+
+    activePortfolio();
+});
